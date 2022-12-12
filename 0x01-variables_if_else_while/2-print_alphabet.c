ead67ca9f69a9282ca3d6a29774b93547f69bc56
Toluwa50 <Toluwaajagunna@gmail.com>
2022-12-12 13:21:38 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,33 +3,20 @@
 #include <stdio.h>
 
 /**
- * main - Prints the last digit of a randomly generated number
- * and whether it is greater than 5, less than 6, or 0.
- *
- *Return: Always 0.
+ * main - main block
+ * Description: Get a random it with 5
+ * Return: 0
  */
 int main(void)
 {
-	int n;
+	char C = 'a';
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	while (C <= 'z')
+	{
+		putchar(C);
+		C++;
+	}
 
-	if ((n % 10) > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n",
-
-				n, n % 10);
-	}
-	else if ((n % 10) < 6 && (n & 10) != 0)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-				n, n % 10);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is 0\n",
-				n, n % 10);
-	}
+	putchar('\n');
 	return (0);
 }
